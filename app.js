ae06f81d2cb844729d038f566779c0e7502bdcee
etfaghaoubeid <etfaghaoubeid@gmail.com>
2019-05-31 11:03:41 +0000

    works but with repetition

--- app.js
+++ app.js
@@ -6,36 +6,37 @@ var xml = fs.readFileSync('./files/xml/file.xml', 'utf8');
 var options = {ignoreComment: true, alwaysChildren: false,spaces:'\t',compact:true,ignoreAttributes:true,};
 var result = convert.xml2json (xml, options);  
 
-
-
-let javaScritObject = JSON.parse(result)
-console.log(util.inspect(javaScritObject, {showHidden: false, depth: null}))
-//console.log(javaScritObject)
-let {INFO_GENERALES,AGENCE,VENTE,APPARTEMENT,
-  LOCALISATION,DEFISCALISATION,INTITULE,
-  COMMENTAIRES,ALUR,VIAGER,IMAGES}=javaScritObject.LISTEPA.BIEN
-exports.INFO_GENERALES = INFO_GENERALES;
-exports.AGENCE = AGENCE;
-exports.VENTE = VENTE
-exports.APPARTEMENT = APPARTEMENT,
-exports.LOCALISATION = LOCALISATION
-exports.DEFISCALISATION = DEFISCALISATION;
-exports.INTITULE = INTITULE;
-exports.COMMENTAIRES = COMMENTAIRES;
-exports.ALUR = ALUR;
-exports.VIAGER = VIAGER;
-exports.IMAGES = IMAGES;
-
-
-
-
-
-
 // write into json File
-fs.writeFile("./files/json/file.json", result, (err) => {
+fs.writeFileSync("./files/file.json", result, (err) => {
   if (err) console.log(err);
   console.log("Successfully Written to File.");
 });
+
+
+
+let javaScritObject = JSON.parse(result)
+//console.log(util.inspect(javaScritObject, {showHidden: false, depth: null}))
+//console.log(javaScritObject)
+let {LISTEPA} =javaScritObject
+exports.LISTEPA = LISTEPA
+// let {INFO_GENERALES,AGENCE,VENTE,APPARTEMENT,
+//   LOCALISATION,DEFISCALISATION,INTITULE,
+//   COMMENTAIRES,ALUR,VIAGER,IMAGES,MAISON}=javaScritObject.LISTEPA.BIEN
+// exports.INFO_GENERALES = INFO_GENERALES;
+// exports.AGENCE = AGENCE;
+// exports.VENTE = VENTE
+// exports.APPARTEMENT = APPARTEMENT,
+// exports.LOCALISATION = LOCALISATION
+// exports.DEFISCALISATION = DEFISCALISATION;
+// exports.INTITULE = INTITULE;
+// exports.COMMENTAIRES = COMMENTAIRES;
+// exports.ALUR = ALUR;
+// exports.VIAGER = VIAGER;
+// exports.IMAGES = IMAGES;
+// exports.MAISON = MAISON
+
+
+
 const {fields} = require('./files/config/fields')
 const {myData} = require('./files/config/data')
 
